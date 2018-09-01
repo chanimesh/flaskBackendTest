0cd913558afb9ffdff7b65d262bc2a86b63e94ba
Animesh <animeshc@thoughtworks.com>
2018-09-01 19:51:48 +0530

    Refactored get all transactions method

--- tms/transactions.py
+++ tms/transactions.py
@@ -7,15 +7,13 @@ from tms.db import get_db
 bp = Blueprint('transactions', __name__, url_prefix='/transactions')
 
 
-@bp.route('/all', methods=('GET', 'POST'))
-def register():
-    if request.method == 'GET':
-        db = get_db()
-        cr = db.execute(
-            'SELECT * FROM transactions')
-        data = cr.fetchall()
-        result = []
-        for row in data:
-            result.append(dict(row))
-        return jsonify(result)
-    return jsonify(errorMessage='something wrong')
+@bp.route('/all', methods=['GET'])
+def all_transactions():
+    db = get_db()
+    cr = db.execute(
+        'SELECT * FROM transactions')
+    data = cr.fetchall()
+    result = []
+    for row in data:
+        result.append(dict(row))
+    return jsonify(result)
